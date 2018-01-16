03e82228b111c10261ad01a58b78fc8287746de8
Flavio Visetti Soares <flavio.visetti@gmail.com>
2018-01-16 21:45:51 -0200

    add i18n and L10n
    include internacionalization into app

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
-from flask import Flask
+from flask import Flask, request
 from config import Config, DevelopmentConfig, ProductionConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,6 +9,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
 
 cfg = '{}{}'.format(os.environ.get('FLASK_CONFIG').capitalize(), 'Config')
 
@@ -18,16 +19,26 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
+
 
 from app import routes, models, errors
-    
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 if not app.debug:
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler = RotatingFileHandler(
+            'logs/microblog.log', maxBytes=10240, backupCount=10)
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
     ))
--- app/email.py
+++ app/email.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from flask_mail import Message
+from flask_babel import _
 from threading import Thread
 from app import mail, app
 
@@ -8,16 +9,18 @@ def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
+
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
     Thread(target=send_async_email, args=(app, msg)).start()
 
+
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email(
-        '[Microblog] Reset your Password',
+        _('[Microblog] Reset your Password'),
         sender=app.config['ADMINS'][0],
         recipients=[user.email],
         text_body=render_template('email/reset_password.txt', user=user, token=token),
